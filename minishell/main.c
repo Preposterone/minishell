82575da9dfa9acd50a9e721d66fa0646e1a0522e
Rolland Steele <rsteele@et-n6.kzn.21-school.ru>
2021-04-18 14:39:41 +0300

    18april-14:38_history

--- minishell/main.c
+++ minishell/main.c
@@ -2,7 +2,7 @@
 #include "minishell.h"
 
 
-//gcc -g main.c work_with_file.c ft_strjoin_for_mas.c ft_strjoin_for_line.c  terminal_two_fun.c terminal_one_fun.c  -ltermcap && ./a.out
+//gcc -g main.c work_with_file.c ft_strjoin_for_mas.c ft_strjoin_for_line.c  terminal_two_fun.c terminal_one_fun.c gnl.c  -ltermcap && ./a.out
 
 int main(int argc, char const *argv[], char const *envp[])
 {
