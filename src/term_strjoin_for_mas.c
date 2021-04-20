099df2300302064b27dd4c69673731ec4c9e443b
Rolland Steele <rsteele@ev-e2.kzn.21-school.ru>
2021-04-20 11:55:28 +0300

    termcap-20april-11:55

---
+++ src/term_strjoin_for_mas.c
@@ -0,0 +1,91 @@
+#include "termcap.h"
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s || s == NULL || !s[i])
+		return (0);
+	while (s[i] && s[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_strlen_mas(char **s)
+{
+	int	i;
+
+	i = 0;
+	if (!s || !s[i] || s[0] == NULL)
+		return (0);
+	while (s[i] && s[i] != NULL)
+		i++;
+	return (i);
+}
+
+char	**strjoin_for_mas(int len, t_for_in_terminal *t, char *line)
+{
+	char	**lin;
+	int		i;
+
+	i = 0;
+	lin = (char **)malloc((len + 1) * sizeof(char *));
+	if (!lin)
+		return (NULL);
+	while (t->mas_his && t->mas_his[i] != NULL && i < len)
+	{
+		lin[i] = ft_strjoin(NULL, t->mas_his[i]);
+		if (t->mas_his[i])
+		{
+			free((void *)t->mas_his[i]);
+			t->mas_his[i] = NULL;
+		}
+		i++;
+	}
+	lin[i] = ft_strjoin(NULL, line);
+	lin[++i] = NULL;
+	if (t->mas_his)
+	{
+		free((void **)t->mas_his);
+		t->mas_his = NULL;
+	}
+	return (lin);
+}
+
+void	*my_memmove(void *dst,
+		const void *src, size_t len)
+{
+	unsigned char	*j;
+	unsigned char	*b;
+
+	j = (unsigned char *)src;
+	b = (unsigned char *)dst;
+	if (!b && !j)
+		return (b);
+	if (b < j)
+	{
+		while (len--)
+			*b++ = *j++;
+	}
+	else
+	{
+		b += len;
+		j += len;
+		while (len--)
+			*--b = *--j;
+	}
+	return (dst);
+}
+
+int	gnl_second(int reader, char **buff, char	**h_const)
+{
+	if (*buff)
+	{
+		free((void *)*buff);
+		*buff = NULL;
+	}
+	if (reader == 0)
+		return (0);
+	return (1);
+}
---
+++ src/term_terminal_main_fun.c
@@ -0,0 +1,129 @@
+#include "termcap.h"
+
+void	del_term(t_for_in_terminal *t)
+{
+	if (t->i == ft_strlen_mas(t->mas_his) && t->s)
+	{
+		if (t->s[0] != 0 && t->s[0] != 10)
+		{
+			tputs(cursor_left, 1, ft_putchar);
+			tputs(tgetstr("dc", 0), 1, ft_putchar);
+		}
+		t->s = str_delet_last_char(t->s);
+	}
+	else if (t->sn)
+	{
+		if (t->sn[0] != 0)
+		{
+			tputs(cursor_left, 1, ft_putchar);
+			tputs(tgetstr("dc", 0), 1, ft_putchar);
+		}
+		t->sn = str_delet_last_char(t->sn);
+	}
+}
+
+void	do_term(t_for_in_terminal *t)
+{
+	ioctl(0, FIONREAD, &t->n);
+	t->l = read(0, t->str, t->n);
+	t->str[t->l] = 0;
+	if (!ft_strcmp(t->str, "\e[A"))
+		up_terminal(t);
+	else if (!ft_strcmp(t->str, "\e[B"))
+		down_term(t);
+	else if (ft_strcmp(t->str, key_backspace) && !ft_strcmp(t->str, "\177"))
+		del_term(t);
+	else if (!ft_strcmp(t->str, "\e[D"))
+		write(1, "left", 0);
+	else if (!ft_strcmp(t->str, "\e[C"))
+		write(1, "right", 0);
+	else
+	{
+		if (t->str[0] == 0)
+			t->i = t->i;
+		else if (t->i == ft_strlen_mas(t->mas_his))
+			t->s = ft_strjoin(t->s, t->str);
+		else if (t->mas_his[t->i])
+			t->sn = ft_strjoin(t->sn, t->str);
+		write (1, t->str, t->l);
+	}
+}
+
+void	while_enter_term(t_for_in_terminal *t)
+{
+	if (ft_strcmp(t->s, "\n"))
+	{
+		if (t->i == ft_strlen_mas(t->mas_his)
+			&& t->s[0] != 0 && t->s[0] != 4 && t->s[0] != 10)
+		{
+			t->mas_his = strjoin_for_mas(ft_strlen_mas(t->mas_his)
+					+ 1, t, t->s);
+			t->j = t->j + 1;
+			t->i = t->j;
+		}
+		else if (t->i == 0 && ft_strlen_mas(t->mas_his) == 0)
+			t->i = t->i;
+		else if (t->mas_his[t->i])
+		{
+			if (t->sn != NULL && t->sn[0] != 0
+				&& t->sn[0] != 4 && t->sn[0] != 10)
+			{
+				t->mas_his = strjoin_for_mas(ft_strlen_mas(t->mas_his)
+						+ 1, t, t->sn);
+				t->j = t->j + 1;
+				t->i = t->j;
+			}
+		}
+	}
+}
+
+void	terminal_while(t_for_in_terminal *t)
+{
+	tputs(save_cursor, 1, ft_putchar);
+	while (1 == 1)
+	{
+		do_term(t);
+		if (!ft_strcmp(t->str, "\n") || !ft_strcmp(t->str, "\4"))
+			break ;
+	}
+	while_enter_term(t);
+	if (t->s)
+	{
+		free((void *)t->s);
+		t->s = NULL;
+	}
+	if (t->sn)
+	{
+		free((void *)t->sn);
+		t->sn = NULL;
+	}
+	write(1, TERMINALNAME, 11);
+	t->i = t->j;
+}
+
+void	terminal(int argc, char const *argv[], char const *envp[])
+{
+	t_for_in_terminal	t;
+
+	t.argc = argc;
+	t.argv = argv;
+	t.envp = envp;
+	tcgetattr(0, &t.term);
+	from_file(&t);
+	t.i = ft_strlen_mas(t.mas_his);
+	t.term.c_lflag &= ~(ECHO);
+	t.term.c_lflag &= ~(ICANON);
+	tcsetattr(0, TCSANOW, &t.term);
+	tgetent(0, t.term_name);
+	t.term_name = "xterm-256color";
+	t.n = 0;
+	t.j = t.i;
+	t.peri = t.i;
+	write(1, &t.str, 100);
+	write(1, TERMINALNAME, 11);
+	while (ft_strcmp(t.str, "\4"))
+		terminal_while(&t);
+	write(1, "\n", 1);
+	file_mas(t.mas_his, t.peri);
+	return ;
+}
