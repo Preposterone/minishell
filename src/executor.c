4a24e5d2ab19d37c12071ebf2d5a73ab7452b8ac
preposterone-at-school21 <aarcelia@student.21-school.ru>
2021-05-09 12:10:44 +0300

    Pipes and redirects integrated + basic cleanup

--- src/executor.c
+++ src/executor.c
@@ -6,7 +6,7 @@
 /*   By: aarcelia <aarcelia@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 12:56:29 by aarcelia          #+#    #+#             */
-/*   Updated: 2021/05/07 21:07:58 by aarcelia         ###   ########.fr       */
+/*   Updated: 2021/05/09 12:10:08 by aarcelia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,18 +116,12 @@ int	executor_pipe(char *cmd, char **args, char *cmdpath, t_envp *envp,
 	{
 		cmd_abs = ft_build_command(cmd, cmdpath);
 		newargs = ft_argappend(args, cmd);
-		// id = fork();
-		// if (id)
-			// wait(&ret); //waitpid? WEXIT?
-		// else
-		// {
-			if (execve(cmd_abs, newargs, envp->sh_envp) == -1)
-			{
-				ft_putendl_fd(cmd, 2);
-				ft_putendl_fd("command not found", 2);
-				exit(127);
-			}
-		// }
+		if (execve(cmd_abs, newargs, envp->sh_envp) == -1)
+		{
+			ft_putendl_fd(cmd, 2);
+			ft_putendl_fd("command not found", 2);
+			exit(127);
+		}
 		ft_freematrix((void **)newargs);
 		free(cmd_abs);
 		free(cmdpath);
@@ -145,7 +139,7 @@ void	ft_update_last_arg(t_for_in_parser **par, t_envp *sh_envp)
 	char	*buf;
 
 	buf = NULL;
-	if (par[0]->key != 1)// || par[0]->previous->arguments)
+	if (par[0]->key != 1)
 		buf = NULL;
 	else
 	{
@@ -200,11 +194,9 @@ static void	ft_exec_cmd(t_for_in_parser **par, t_envp *sh_envp, t_for_in_termina
 }
 
 void	ft_exec_cmd_pipe(t_for_in_parser **par, t_envp *sh_envp,
-						t_for_in_terminal *term_props, t_pipe_data *pipe_data)
+						t_for_in_terminal *term_props)
 {
 	char	*cmdpath;
-	(void)pipe_data;
-	/*
 	if ((*par)->output > 0)
 	{
 		// replace stdout with output fd
@@ -217,22 +209,25 @@ void	ft_exec_cmd_pipe(t_for_in_parser **par, t_envp *sh_envp,
 		dup2((*par)->input, 0);
 		close((*par)->input);
 	}
-	*/
-	//reading contents of all the shit
-	// (void)term_props;
-	// (void)sh_envp;
-
-	// fprintf(stderr, "Pipe data:\nchild index: %d, pipe_fds[0]:'%d'[1]:'%d', cmd: '%s'\n",pipe_data->ch_index, pipe_data->pipe_fd[pipe_data->ch_index][0],pipe_data->pipe_fd[pipe_data->ch_index][1], (*par)->arguments[0]);
+/*
+//child debug start
+	fprintf(stderr, "****\nPipe data:\nchild index: '%d', pipe_fds[0]:'%d'[1]:'%d', cmd: '%s'\n"
+	"Parsed OUT: '%d', Parsed IN: '%d'\n",
+	pipe_data->ch_index, pipe_data->pipe_fd[pipe_data->ch_index][0],
+	pipe_data->pipe_fd[pipe_data->ch_index][1], (*par)->arguments[0],
+	(*par)->output, (*par)->input);
+//child debug end
+ */
 	cmdpath = expander((*par)->arguments[0], sh_envp->sh_path);
 	executor_pipe(	(*par)->arguments[0],	//cmd
 							&(*par)->arguments[1],		//args from cmdline
 							cmdpath,					//executable filepath
 							sh_envp,					//settings for shell
-							term_props);				//props for exit (history)
+							term_props);				//props for e	xit (history)
 
 }
 
-//TODO: stuff for pipes,
+//TODO: actual error management
 //maybe think about remaking expander into substring replacement for PATH elements
 
 void	executor_secretary(t_for_in_parser **par, t_envp *sh_envp,
@@ -244,9 +239,6 @@ void	executor_secretary(t_for_in_parser **par, t_envp *sh_envp,
 	t_for_in_parser *par_head;
 
 	ft_update_last_arg(par, sh_envp);	//should this be done here?
-	// ft_putendl_fd(ft_get_envp_elem("_=", sh_envp), 1);	//print current last arg
-	// ft_print_arr(sh_envp->sh_envp);	//print current envp
-	// int output_fd =	open("out_file", O_CREAT | O_APPEND | O_RDWR, 00644);
 	num_cmds = (*par)->key;
 	if (num_cmds > 1)
 	{
@@ -257,15 +249,13 @@ void	executor_secretary(t_for_in_parser **par, t_envp *sh_envp,
 			if (pipe(pipe_data.pipe_fd[i]) == -1)
 			{
 				perror("PIPE FAILED");
-				exit(0);
+				exit(112);
 			}
-		}	//populate pipes
+		}
 		par_head = (*par);
 
 		while (par_head->key != 1)
 			par_head = par_head->previous;	//move head to 1st parsed command
-		// par_head = (*par)->previous;
-
 		pipe_data.ch_index = -1;
 		while(++pipe_data.ch_index < num_cmds)
 		{
@@ -278,38 +268,26 @@ void	executor_secretary(t_for_in_parser **par, t_envp *sh_envp,
 			else if (pipe_data.ch_id[pipe_data.ch_index] == 0)
 			{	//child code
 				if (pipe_data.ch_index > 0)
-				{
 					dup2(pipe_data.pipe_fd[pipe_data.ch_index - 1][0], 0);
-				}
 				if (pipe_data.ch_index < num_cmds - 1)
-				{
 					dup2(pipe_data.pipe_fd[pipe_data.ch_index][1], 1);
-				}
 				else
-				{
 					dup2(sh_envp->truefd1, STDOUT_FILENO);
-				}
 				ft_close_all_pipes(pipe_data.pipe_fd, num_cmds);
-				//do execve here but without fork
-				ft_exec_cmd_pipe(&par_head, sh_envp, term_props, &pipe_data);
+				ft_exec_cmd_pipe(&par_head, sh_envp, term_props);
 				exit(0);
 			}
 			else
-			{
 				close(pipe_data.pipe_fd[pipe_data.ch_index][1]);
-			}
 			par_head = par_head->next;
 		}
 		ft_close_all_pipes(pipe_data.pipe_fd, num_cmds);
 		i = -1;
 		while (++i < num_cmds)
-		{
-			wait(NULL);
-		}
+			wait(NULL);	//TODO: waitpid with exit status?
 		dup2(sh_envp->truefd0, 0);
 		dup2(sh_envp->truefd1, 1);
 	}
-	else {
+	else
 		ft_exec_cmd(par, sh_envp, term_props);
-	}
 }
