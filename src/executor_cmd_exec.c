8a458259dfe4fc3f65149ebbde8390ef083bdaea
preposterone-at-school21 <aarcelia@student.21-school.ru>
2021-05-10 11:07:54 +0300

    Exit logic rudimentary implementation2

---
+++ src/executor_cmd_exec.c
@@ -0,0 +1,60 @@
+#include "minishell.h"
+
+
+//if output_fd != -1, do dup2(output_fd, 1); close (output_fd);
+int	executor_pipe(char **args, char *cmdpath, t_envp *envp,
+					 t_for_in_terminal *term_props)
+{
+	int		ret;
+	char	*cmd_abs;
+	char	**newargs;
+
+	ret = 0;
+	if (!args[0])	//prevent segfault if no actual cmd is given
+		ret = 0;
+	else if (ft_isbuiltin(args[0]))
+		ret = ft_do_builtin(args[0], &args[1], envp, term_props); //do builtin
+	else
+	{
+		cmd_abs = ft_build_command(args[0], cmdpath);
+		newargs = ft_argappend(&args[1], args[0]);
+		if (execve(cmd_abs, newargs, envp->sh_envp) == -1)
+			exit_minishell(args[0], MSH_CMD_NOT_FOUND, term_props);
+	}
+	return (ret);
+}
+
+void	ft_exec_cmd_pipe(t_for_in_parser **par, t_envp *sh_envp,
+						 t_for_in_terminal *term_props, t_pipe_data *pipe_data)
+{
+	char	*cmdpath;
+	if ((*par)->output > 0)
+	{
+		// replace stdout with output fd
+		dup2((*par)->output, 1);
+		close((*par)->output);
+	}
+	if ((*par)->input > 0)
+	{
+		// replace stdin with input fd
+		dup2((*par)->input, 0);
+		close((*par)->input);
+	}
+	(void)pipe_data;
+/*
+//child debug start
+	fprintf(stderr, "****\nPipe data:\nchild index: '%d', pipe_fds[0]:'%d'[1]:'%d', cmd: '%s'\n"
+	"Parsed OUT: '%d', Parsed IN: '%d'\n",
+	pipe_data->ch_index, pipe_data->pipe_fd[pipe_data->ch_index][0],
+	pipe_data->pipe_fd[pipe_data->ch_index][1], (*par)->arguments[0],
+	(*par)->output, (*par)->input);
+//child debug end
+ */
+	cmdpath = expander((*par)->arguments[0], sh_envp->sh_path);
+	executor_pipe(
+			&(*par)->arguments[0],		//args from cmdline
+			cmdpath,					//executable filepath
+			sh_envp,					//settings for shell
+			term_props);				//props for exit (history)
+
+}
