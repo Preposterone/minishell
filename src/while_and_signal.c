799f1e6281d2762c3fcce51a9177dd24f078ed5f
Ilyavii <v.ashaev98@mail.ru>
2021-05-13 18:46:54 +0300

    Norma_H

--- src/while_and_signal.c
+++ src/while_and_signal.c
@@ -1,5 +1,9 @@
 #include "minishell.h"
 
+void	ft_signal_slesh(int sig);
+
+void	ft_signal_c(int sig);
+
 void	while_enter_term2(t_for_in_terminal *t, t_envp *sh_envp)
 {
 	t->mas_his = strjoin_for_mas(term_strlen_mas(t->mas_his) + 1, t, t->sn);
@@ -35,16 +39,38 @@ void	while_enter_term(t_for_in_terminal *t, t_envp *sh_envp)
 	}
 }
 
-void	ft_signal_slesh(void)
+void	terminal_while(t_for_in_terminal *t, t_envp *sh_envp)
+{
+	tputs(save_cursor, 1, term_putchar);
+	g_all.key_ctr = 0;
+	t->key_main = 1;
+	while (t->key_main == 1)
+	{
+		g_all.key_ctr = 1;
+		signal(SIGQUIT, ft_signal_slesh);
+		signal(SIGINT, ft_signal_c);
+		terminal_while2(t);
+		if (!term_strcmp(t->str, "\n\0"))
+		{
+			write(1, "\n", 1);
+			t->key_main = 0;
+		}
+	}
+	terminal_while3(t, sh_envp);
+}
+
+void	ft_signal_slesh(int sig)
 {
 	if (g_all.key_ctr == 0)
 		write(1, "Quit: 3\n", ft_strlen("Quit: 3\n"));
 	g_all.key_signal = 2;
+	sig = 1;
 }
 
-void	ft_signal_c(void)
+void	ft_signal_c(int sig)
 {
 	write(1, "\n", 1);
 	g_all.key_signal = 1;
+	sig = 1;
 	g_all.exit_code = 1;
 }
